f0f2411c1dc4709889de21cb99cd3979edf08cee
Niko Vrdoljak <niko.vrdoljak@hotmail.com>
2024-10-31 19:25:19 +0100

    wtf

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+class NameForm(FlaskForm):
+    name = StringField("Ime", validators=[DataRequired()])
+    submit = SubmitField("Pošalji")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    name = ""
+    if request.method == "POST":
+        # Ako je POST, podatke dobijemo iz request.form
+        name = request.form.get("name", "")
+    else:
+        # Ako je GET, podatke dobijemo iz request.args
+        name = request.args.get("name", "") 
+    return render_template("index.html", name = name) 
