7f73be949ba915bf06276129a8c9a8509b068a42
Niko Vrdoljak <niko.vrdoljak@hotmail.com>
2024-10-31 19:54:32 +0100

    wtf completed

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.secret_key = 'tajni_ključ'  # Ovo postavljamo kao tajni ključ za sigurnost
 bootstrap = Bootstrap5(app)
 
 class NameForm(FlaskForm):
@@ -13,11 +14,11 @@ class NameForm(FlaskForm):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    name = ""
-    if request.method == "POST":
-        # Ako je POST, podatke dobijemo iz request.form
-        name = request.form.get("name", "")
-    else:
-        # Ako je GET, podatke dobijemo iz request.args
-        name = request.args.get("name", "") 
-    return render_template("index.html", name = name) 
+    form = NameForm()
+    if form.validate_on_submit():  # Provjerava li je obrazac uspješno poslan i validiran
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Promijenili ste ime!', 'success')
+        session['name'] = form.name.data # pohranjuje ime u sesiju
+        return redirect(url_for('index'))  # Preusmjeravamo na istu rutu 'index'
+    return render_template('index.html', name = session.get('name'), form = form)
