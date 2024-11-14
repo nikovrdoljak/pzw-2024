a112196cc0df0a3baf2702a3047c36b0c044e70c
Niko Vrdoljak <niko.vrdoljak@hotmail.com>
2024-11-14 19:25:34 +0100

    Predavanje 3 završeno

--- app.py
+++ app.py
@@ -1,24 +1,133 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, DateField, FileField, SubmitField, RadioField
+from wtforms.validators import DataRequired, Length
+from flask_wtf.file import FileAllowed
+from datetime import datetime, timezone
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import gridfs
+import markdown
+
 
 app = Flask(__name__)
 app.secret_key = 'tajni_ključ'  # Ovo postavljamo kao tajni ključ za sigurnost
 bootstrap = Bootstrap5(app)
+client = MongoClient('mongodb://localhost:27017/')
+db = client['pzw_blog_database']
+posts_collection = db['posts']
+fs = gridfs.GridFS(db)
 
 class NameForm(FlaskForm):
     name = StringField("Ime", validators=[DataRequired()])
     submit = SubmitField("Pošalji")
 
+class BlogPostForm(FlaskForm):
+    title = StringField('Naslov', validators=[DataRequired(), Length(min=5, max=100)])
+    content = TextAreaField('Sadržaj', render_kw={"id": "markdown-editor"})
+    author = StringField('Autor', validators=[DataRequired()])
+    date = DateField('Datum', default=datetime.today)
+    status = RadioField('Status', choices=[('draft', 'Skica'), ('published', 'Objavljeno')], default='draft')
+    tags = StringField('Oznake', render_kw={"id": "tags"})
+    image = FileField('Blog Image', validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Samo slike!')])
+    submit = SubmitField('Spremi')
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():  # Provjerava li je obrazac uspješno poslan i validiran
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Promijenili ste ime!', 'success')
-        session['name'] = form.name.data # pohranjuje ime u sesiju
-        return redirect(url_for('index'))  # Preusmjeravamo na istu rutu 'index'
-    return render_template('index.html', name = session.get('name'), form = form)
+    published_posts = posts_collection.find({"status": "published"}).sort('date', -1)
+    return render_template('index.html', posts = published_posts)
+
+@app.route('/blog/create', methods=["get", "post"])
+def post_create():
+    form = BlogPostForm()
+    if form.validate_on_submit():
+        image_id = save_image_to_gridfs(request, fs)
+        post = {
+            'title': form.title.data,
+            'content': form.content.data,
+            'author': form.author.data,
+            'status': form.status.data,
+            'date': datetime.combine(form.date.data, datetime.min.time()),
+            'tags': form.tags.data,
+            'image_id': image_id,
+            'date_created': datetime.utcnow()
+        }
+        posts_collection.insert_one(post)
+        flash('Članak je uspješno upisan.', 'success')
+        return redirect(url_for('index'))
+    return render_template('blog_edit.html', form=form)
+
+@app.route('/blog/<post_id>')
+def post_view(post_id):
+    post = posts_collection.find_one({'_id': ObjectId(post_id)})
+
+    if not post:
+        flash("Članak nije pronađen!", "danger")
+        return redirect(url_for('index'))
+
+    return render_template('blog_view.html', post=post)
+
+@app.route('/blog/edit/<post_id>', methods=["get", "post"])
+def post_edit(post_id):
+    form = BlogPostForm()
+    post = posts_collection.find_one({"_id": ObjectId(post_id)})
+
+    if request.method == 'GET':
+        form.title.data = post['title']
+        form.content.data = post['content']
+        form.author.data = post['author']
+        form.date.data = post['date']
+        form.tags.data = post['tags']
+        form.status.data = post['status']
+    elif form.validate_on_submit():
+        posts_collection.update_one(
+            {"_id": ObjectId(post_id)},
+            {"$set": {
+                'title': form.title.data,
+                'content': form.content.data,
+                'date': datetime.combine(form.date.data, datetime.min.time()),
+                'tags': form.tags.data,
+                'status': form.status.data,
+                'date_updated': datetime.utcnow()
+            }}
+        )
+        image_id = save_image_to_gridfs(request, fs)
+        if image_id != None:
+            posts_collection.update_one(
+            {"_id": ObjectId(post_id)},
+            {"$set": {
+                'image_id': image_id,
+            }}
+        )        
+        flash('Članak je uspješno ažuriran.', 'success')
+        return redirect(url_for('post_view', post_id = post_id))
+    else:
+        flash('Dogodila se greška!', category='warning')
+    return render_template('blog_edit.html', form=form)
+
+@app.route('/blog/delete/<post_id>', methods=['POST'])
+def delete_post(post_id):
+    posts_collection.delete_one({"_id": ObjectId(post_id)})
+    flash('Članak je uspješno obrisan.', 'success')
+    return redirect(url_for('index'))
+
+def save_image_to_gridfs(request, fs):
+    if 'image' in request.files:
+        image = request.files['image']
+        if image.filename != '':
+            image_id = fs.put(image, filename=image.filename)
+        else:
+            image_id = None
+    else:
+        image_id = None
+    return image_id
+
+@app.route('/image/<image_id>')
+def serve_image(image_id):
+    image = fs.get(ObjectId(image_id))
+    return image.read(), 200, {'Content-Type': 'image/jpeg'}
+
+@app.template_filter('markdown')
+def markdown_filter(text):
+    return markdown.markdown(text)
